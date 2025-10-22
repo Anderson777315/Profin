c76b90fd23a035c7afec57dae9c132f31b5a3fea
ANDERSON RAMOS <aramosb3@miumg.edu.gt>
2025-10-22 11:10:16 -0600

    Actualización full

--- proyecto/app/models/index.js
+++ proyecto/app/models/index.js
@@ -32,31 +32,12 @@ db.inventarioBoletos = require("./inventario.model.js")(sequelize, Sequelize);
 db.venta = require("./venta.model.js")(sequelize, Sequelize);
 db.detalleVenta = require("./detalleVenta.model.js")(sequelize, Sequelize);
 
-
-
-
-/*
-// Relaciones
-// Usuarios y Ventas
 db.usuario.hasMany(db.venta, { foreignKey: 'id_vendedor' });
 db.venta.belongsTo(db.usuario, { foreignKey: 'id_vendedor' });
-// Partidos e Inventario
-db.partido.hasMany(db.inventarioBoletos, { foreignKey: 'id_partido' });
-db.inventarioBoletos.belongsTo(db.partido, { foreignKey: 'id_partido' });
-// Localidades e Inventario
-db.localidad.hasMany(db.inventarioBoletos, { foreignKey: 'id_localidad' });
-db.inventarioBoletos.belongsTo(db.localidad, { foreignKey: 'id_localidad' });
+
 // Ventas y DetalleVentas
 db.venta.hasMany(db.detalleVenta, { foreignKey: 'id_venta' });
 db.detalleVenta.belongsTo(db.venta, { foreignKey: 'id_venta' });
-// Partidos y DetalleVentas
-db.partido.hasMany(db.detalleVenta, { foreignKey: 'id_partido' });
-db.detalleVenta.belongsTo(db.partido, { foreignKey: 'id_partido' });
-// Localidades y DetalleVentas
-db.localidad.hasMany(db.detalleVenta, { foreignKey: 'id_localidad' });
-db.detalleVenta.belongsTo(db.localidad, { foreignKey: 'id_localidad' });
-// Exportamos db para poder usarlo en otros archivos
-module.exports = db;*/
 db.usuario.hasMany(db.venta, { foreignKey: 'id_vendedor' });
 db.venta.belongsTo(db.usuario, { foreignKey: 'id_vendedor' });
 db.venta.hasMany(db.detalleVenta, { foreignKey: 'id_venta' });
