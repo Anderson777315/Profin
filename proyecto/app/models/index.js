c2dce18efc071d2fd6498b7a7149173652ad077f
ANDERSON RAMOS <aramosb3@miumg.edu.gt>
2025-10-28 23:54:06 -0600

    Actualización full

--- proyecto/app/models/index.js
+++ proyecto/app/models/index.js
@@ -29,32 +29,34 @@ db.sequelize = sequelize;
 db.usuario = require("./usuario.model.js")(sequelize, Sequelize);
 db.partido = require("./partido.model.js")(sequelize, Sequelize);
 db.localidad = require("./localidades.model.js")(sequelize, Sequelize);
+db.partido_localidad = require("./partidolocalidad.model.js")(sequelize, Sequelize);
 db.inventarioBoletos = require("./inventario.model.js")(sequelize, Sequelize);
 db.venta = require("./venta.model.js")(sequelize, Sequelize);
 db.detalleVenta = require("./detalleVenta.model.js")(sequelize, Sequelize);
-db.partido_localidad = require("./partidolocalidad.model.js")(sequelize, Sequelize);
 
-// Usuario ↔ Ventas
+//RELACIONES 
+
+// Usuario  Ventas
 db.usuario.hasMany(db.venta, { foreignKey: 'id_vendedor' });
 db.venta.belongsTo(db.usuario, { foreignKey: 'id_vendedor' });
 
-// Venta ↔ DetalleVenta
+// Venta / DetalleVenta
 db.venta.hasMany(db.detalleVenta, { foreignKey: 'id_venta' });
 db.detalleVenta.belongsTo(db.venta, { foreignKey: 'id_venta' });
 
-// Partido ↔ PartidoLocalidad
+// Partido / PartidoLocalidad
 db.partido.hasMany(db.partido_localidad, { foreignKey: 'id_partido' });
 db.partido_localidad.belongsTo(db.partido, { foreignKey: 'id_partido' });
 
-// Localidad ↔ PartidoLocalidad (usando nombre en vez de id)
+// Localidad / PartidoLocalidad (usando nombre)
 db.localidad.hasMany(db.partido_localidad, { foreignKey: 'nombre', sourceKey: 'nombre' });
 db.partido_localidad.belongsTo(db.localidad, { foreignKey: 'nombre', targetKey: 'nombre' });
 
-// InventarioBoletos ↔ PartidoLocalidad
-db.partido_localidad.hasMany(db.inventarioBoletos, { foreignKey: 'nombre_localidad', sourceKey: 'nombre' });
-db.inventarioBoletos.belongsTo(db.partido_localidad, { foreignKey: 'nombre_localidad', targetKey: 'nombre' });
+// PartidoLocalidad / InventarioBoletos (relación por ID)
+db.partido_localidad.hasOne(db.inventarioBoletos, { foreignKey: 'id_partido_localidad' });
+db.inventarioBoletos.belongsTo(db.partido_localidad, { foreignKey: 'id_partido_localidad' });
 
-// InventarioBoletos ↔ Venta y DetalleVenta
+// InventarioBoletos / Venta y DetalleVenta (para consultas rápidas)
 db.inventarioBoletos.hasMany(db.detalleVenta, { foreignKey: 'nombre_partido', sourceKey: 'nombre_partido' });
 db.detalleVenta.belongsTo(db.inventarioBoletos, { foreignKey: 'nombre_partido', targetKey: 'nombre_partido' });
 
